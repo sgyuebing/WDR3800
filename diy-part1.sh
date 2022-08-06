b8ba6e3f726775adfb1485071ffe4e2ba06141a6
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2022-08-06 23:04:01 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,23 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+ sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
+#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
+sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
+
+####### Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf #delete original argon theme
+#echo 'src-git ArgonTheme https://github.com/jerrykuku/luci-theme-argon.git' >> 'feeds.conf.default' #repleace with JerryKuKu’s Argon Theme
+#echo 'src-git ArgonThemeConfig https://github.com/jerrykuku/luci-app-argon-config.git' >> 'feeds.conf.default' #add JerryKuKu’s Argon Config Feature
+
+rm -rf ./feeds/luci/themes/luci-theme-argon.
+#rm -rf package/lean/luci-theme-argon  #删除原Argon主题
+#git clone -b master https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon #加入JerryKuKu’s Argon
+#git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config #加入JerryKuKu’s Argon Config
+#cd ..
+#cd ..
--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+###### Modify default IP
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+###### Modify hostname
+sed -i 's/OpenWrt/NewifiD1/g' package/base-files/files/bin/config_generate
+
+####### Modify the version number
+sed -i "s/OpenWrt /Fndsz build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+####### Set argon as default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
